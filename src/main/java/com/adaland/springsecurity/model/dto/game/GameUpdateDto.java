de535717f5cbd8f9b3afbc3592bd35b7cf101f69
adaland <to.ada.boczar@gmail.com>
2024-05-24 16:07:42 +0200

    [MODEL] added builder

--- src/main/java/com/adaland/springsecurity/model/dto/game/GameUpdateDto.java
+++ src/main/java/com/adaland/springsecurity/model/dto/game/GameUpdateDto.java
@@ -2,9 +2,11 @@ package com.adaland.springsecurity.model.dto.game;
 
 
 import com.adaland.springsecurity.model.dao.GameStatus;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class GameUpdateDto {
 
     private String title;
