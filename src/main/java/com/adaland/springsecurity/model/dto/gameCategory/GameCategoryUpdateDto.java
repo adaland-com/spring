efbd25993b67bb551b300b83cbb4e5a23c39644a
adaland <to.ada.boczar@gmail.com>
2024-05-24 16:10:05 +0200

    [MODEL] added GameCategoryUpdateDto.java

---
+++ src/main/java/com/adaland/springsecurity/model/dto/gameCategory/GameCategoryUpdateDto.java
@@ -0,0 +1,17 @@
+package com.adaland.springsecurity.model.dto.gameCategory;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class GameCategoryUpdateDto {
+
+
+    private String name;
+
+
+}
