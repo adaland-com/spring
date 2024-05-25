5fb5852a719fbc9c4e079cbdad90e880abd1cbdd
adaland <to.ada.boczar@gmail.com>
2024-05-25 12:14:34 +0200

    [CLEAN] exception changed to NoSuchElement

--- src/main/java/com/adaland/springsecurity/exception/EntityNotFoundException.java
+++ src/main/java/com/adaland/springsecurity/exception/EntityNotFoundException.java
@@ -3,8 +3,10 @@ package com.adaland.springsecurity.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.NoSuchElementException;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class EntityNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends NoSuchElementException {
 
     public static final String ENTITY_NOT_FOUND_MESSAGE = "Not found %s";
     public static final String ENTITY_CLIENT_NOT_FOUND = "Client not found";
