7ef757687973c9cfd42863d3f1fd5ad13b6ac90e
adaland <to.ada.boczar@gmail.com>
2024-05-26 20:25:10 +0200

    [CLEAN] deleted unused file

--- src/main/java/com/adaland/springsecurity/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.adaland.springsecurity.repository;
-
-import com.adaland.springsecurity.model.dao.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
