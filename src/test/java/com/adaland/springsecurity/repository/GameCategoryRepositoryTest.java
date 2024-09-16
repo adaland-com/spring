6ee0ff8027bbdb9932719848ee36ed416068abe4
adaland <to.ada.boczar@gmail.com>
2024-09-16 16:10:30 +0200

    [TESTS] Repository tests

--- src/test/java/com/adaland/springsecurity/repository/GameCategoryRepositoryTest.java
+++ src/test/java/com/adaland/springsecurity/repository/GameCategoryRepositoryTest.java
@@ -1,17 +1,13 @@
 package com.adaland.springsecurity.repository;
 
-import com.adaland.springsecurity.model.dao.Game;
 import com.adaland.springsecurity.model.dao.GameCategory;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,42 +17,39 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class GameCategoryRepositoryTest {
-    
 
     @Autowired
     private GameCategoryRepository gameCategoryRepository;
 
     String existingCategoryName = "test category";
-    
 
     @Test
     public void givenGameCategoryObject_whenSave_thenReturnSavedGameCategory() {
-        String gameCategoryName="cooperative";
+        String gameCategoryName = "cooperative";
         GameCategory gameCategory = GameCategory.builder()
                 .name(gameCategoryName)
                 .build();
 
         GameCategory savedGameCategory = gameCategoryRepository.save(gameCategory);
-        
+
         assertThat(savedGameCategory).isNotNull();
         assertThat(savedGameCategory.getName()).isEqualTo(gameCategoryName);
     }
 
     @Test
     public void given_whenFindAll_thenReturnListGameCategory() {
-        String gameCategoryName="cooperative";
+        String gameCategoryName = "cooperative";
         GameCategory gameCategory = GameCategory.builder()
                 .name(gameCategoryName)
                 .build();
 
-        String gameCategoryName2="family";
+        String gameCategoryName2 = "family";
         GameCategory gameCategory2 = GameCategory.builder()
                 .name(gameCategoryName2)
                 .build();
 
         GameCategory savedGameCategory = gameCategoryRepository.save(gameCategory);
         GameCategory savedGameCategory2 = gameCategoryRepository.save(gameCategory2);
-        
 
         List<GameCategory> orderList = gameCategoryRepository.findAll();
 
@@ -65,14 +58,13 @@ class GameCategoryRepositoryTest {
 
     @Test
     public void givenGameCategoryObject_whenFindById_thenReturnGameCategory() {
-        
-        String gameCategoryName="family";
+
+        String gameCategoryName = "family";
         GameCategory gameCategory = GameCategory.builder()
                 .name(gameCategoryName)
                 .build();
 
         GameCategory savedGameCategory = gameCategoryRepository.save(gameCategory);
-        
         GameCategory foundGameCategory = gameCategoryRepository.findById(savedGameCategory.getId()).orElse(null);
 
         assertThat(foundGameCategory).isNotNull();
@@ -82,11 +74,11 @@ class GameCategoryRepositoryTest {
 
     @Test
     public void givenGameCategoryObject_whenUpdateGameCategory_thenReturnUpdatedGameCategory() {
-        String gameCategoryName="family";
+        String gameCategoryName = "family";
         GameCategory gameCategory = GameCategory.builder()
                 .name(gameCategoryName)
                 .build();
-        String gameCategoryUpdatedName="logical";
+        String gameCategoryUpdatedName = "logical";
 
         GameCategory savedGameCategory = gameCategoryRepository.save(gameCategory);
         GameCategory foundGameCategory = gameCategoryRepository.findById(savedGameCategory.getId()).orElse(null);
@@ -100,27 +92,25 @@ class GameCategoryRepositoryTest {
     }
 
 
-
     @Test
     public void whenFindByNonExistingGameCategory_thenThrowsException() {
-      
+
         String gameCategoryName = "nonexisting";
-     
+
         Optional<GameCategory> found = gameCategoryRepository.findByName(gameCategoryName);
-        
+
         assertTrue(found.isEmpty());
     }
 
     @Test
     public void givenGameCategoryObject_whenDelete_thenReturnGameCategoryIsEmpty() {
 
-        String gameCategoryName="logical";
+        String gameCategoryName = "logical";
         GameCategory gameCategory = GameCategory.builder()
                 .name(gameCategoryName)
                 .build();
 
         gameCategoryRepository.save(gameCategory);
-
         gameCategoryRepository.deleteById(gameCategory.getId());
 
 
